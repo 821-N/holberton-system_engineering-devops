df27119caac4733df312cbaf8eddc27ef3ba1ece
821-N <nathaniel.marofsky@gmail.com>
2020-02-03 22:31:21 -0500

    fix 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,16 +7,19 @@ from sys import argv
 from json import loads
 
 
-def get(path):
-    response = requests.get("https://jsonplaceholder.typicode.com/"+path)
-    return loads(response.text)
+if __name__ == "__main__":
+    def get(path):
+        response = requests.get("https://jsonplaceholder.typicode.com/"+path)
+        return loads(response.text)
 
-user = get("users/"+argv[1])
-todos = get("users/"+argv[1]+"/todos")
-done = sum(item['completed'] for item in todos)
+    user = get("users/"+argv[1])
+    todos = get("todos?userId="+argv[1])
+    done = sum(item['completed'] for item in todos)
 
-p = print
-p("Employee %s is done with tasks(%d/%d)" % (user['name'], done, len(todos)))
-for item in todos:
-    if item['completed']:
-        print("\t"+item['title'])
+    print(
+        "Employee %s is done with tasks(%d/%d)" %
+        (user['name'], done, len(todos))
+    )
+    for item in todos:
+        if item['completed']:
+            print("\t "+item['title'])
